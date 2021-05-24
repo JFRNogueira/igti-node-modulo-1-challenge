226e3dbb68185cd56e953f56aaef200ae1ac4fc5
Johannes Nogueira <johannes.nogueira@gmail.com>
2021-05-24 19:18:01 -0300

    create routes

---
+++ src/routes/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const { createOneOrder, updateOneOrder } = require('../services/models.service');
+
+app.post('/delivery/createOneOrder', (req, res) => {
+  return createOneOrder(req, res)
+})
+app.patch('/delivery/updateOneOrder', (req, res) => {
+  return updateOneOrder(req, res)
+})
+
+
+module.exports = app
